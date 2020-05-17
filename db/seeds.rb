47f888fd46254bac1a42edbed74d7b07f4421935
zouantchaw <44246692+zouantchaw@users.noreply.github.com>
2020-05-17 18:43:40 -0400

    New seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,6 @@
-require_relative '../config/environment'
 
-names = %w[nancy bob sam ben wiel meghan evan adam]
-
-names.each do |name|
-    email = name + "@gmail.com"
-    password = name + "123"
-    user = User.new(name: name, email: email, password: password)
-    1.times do
-    property = user.properties.build(address: Faker::Address.full_address, description: Faker::Quote.famous_last_words, rent:Faker::Number.decimal(l_digits: 3, r_digits: 3).to_s)
-    user.save 
+    15.times do
     tenant = Tenant.new(name: Faker::Name.name, age: Faker::Number.within(range: 21..100).to_s, occupation: Faker::Job.title)
     tenant.save 
     end 
     
-end 
